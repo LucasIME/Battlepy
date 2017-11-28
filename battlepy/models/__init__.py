1669c2af27c914077f317193e77ee31279bcd62a
Lucas Meireles <meireles31@msn.com>
2017-11-28 18:06:21 +0000

    Added __init__.py to models folder

---
+++ battlepy/models/__init__.py
@@ -0,0 +1,8 @@
+from .basebrobject import BaseBRObject
+from .match import Match
+from .participant import Participant
+from .player import Player
+from .roster import Roster
+from .round import Round
+from .team import Team
+
